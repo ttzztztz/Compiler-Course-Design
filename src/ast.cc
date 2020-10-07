1ed4224b38bad0fe4405f9004f6591e8f3d39f56
ttzztztz <ttzztztz@outlook.com>
2020-10-07 16:51:04 +0800

    refactor

--- src/ast.cc
+++ src/ast.cc
@@ -3,7 +3,7 @@
 
 Operation::Operation() : data(0), kind(0), type(0), level(0) {}
 CodeNode::CodeNode() : op(0), next(nullptr), prior(nullptr) {}
-ASTNode::ASTNode() : kind(0), ptr{nullptr, nullptr, nullptr, nullptr}, place(0), type(0), pos(0), num(0), code(nullptr) {}
+ASTNode::ASTNode() : kind(0), ptr{nullptr, nullptr, nullptr, nullptr}, idx(0), type(0), pos(0), num(0), code(nullptr) {}
 Symbol::Symbol() : level(0), type(0), paramnum(0), flag(0), idx(0) {}
 
 extern vector<Symbol> symbol_table;
