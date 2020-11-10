95e9c7f46fcfee1e88b3a7f8e1e7b8756ac74e3b
ttzztztz <ttzztztz@outlook.com>
2020-11-10 09:34:31 +0800

    fix: var name that cause err

--- src/functions.cc
+++ src/functions.cc
@@ -1,25 +1,25 @@
 #include "compiler.h"
 
-tuple<Function *, FunctionType *, Function *, FunctionType *> inject_print_function(LLVMContext &ctx, IRBuilder<> &builder, Module &module)
+tuple<Function *, FunctionType *, Function *, FunctionType *> inject_print_function(LLVMContext &ctx, IRBuilder<> &builder, Module &_module)
 {
     vector<Type *> printfArgsTypes({Type::getInt8PtrTy(ctx)});
 
-    Function *printf_func = module.getFunction("printf");
+    Function *printf_func = _module.getFunction("printf");
     FunctionType *printf_func_type = FunctionType::get(
-        Type::getInt32Ty(module.getContext()),
-        {Type::getInt8PtrTy(module.getContext())},
+        Type::getInt32Ty(_module.getContext()),
+        {Type::getInt8PtrTy(_module.getContext())},
         true);
     if (printf_func == nullptr)
     {
-        printf_func = Function::Create(printf_func_type, GlobalValue::ExternalLinkage, "printf", module);
+        printf_func = Function::Create(printf_func_type, GlobalValue::ExternalLinkage, "printf", _module);
         printf_func->setCallingConv(llvm::CallingConv::C);
     }
 
     FunctionType *print_int_func_type = FunctionType::get(
         Type::getInt32Ty(ctx),
-        {Type::getInt32Ty(module.getContext())},
+        {Type::getInt32Ty(_module.getContext())},
         false);
-    Function *print_int_func = Function::Create(print_int_func_type, GlobalValue::ExternalLinkage, "print_int", module);
+    Function *print_int_func = Function::Create(print_int_func_type, GlobalValue::ExternalLinkage, "print_int", _module);
     BasicBlock *block = BasicBlock::Create(ctx, "", print_int_func);
     IRBuilder<> block_builder(block);
 
